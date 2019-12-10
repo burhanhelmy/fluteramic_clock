de960ce1a7aa627a98ccfa9f277ae17be83622fb
Burhan <bhbj@syntronic.com>
2019-12-11 01:04:22 +0800

    implemented session

--- lib/panoramic/color_const.dart
+++ lib/panoramic/color_const.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/painting.dart';
 
-ColorSwatch skyColors = ColorSwatch(
-    0, {10: Color(0xFFB1EAFF), 20: Color(0xFFB1EAFE), 30: Color(0xFFB1EAFE)});
+ColorSwatch skyColorsPrimary = ColorSwatch(
+  1,
+  {
+    0: Color(0xFFFFE4B1), //pagi
+    25: Color(0xFFB1EAFF), // tengah hari
+    50: Color(0xFFFEB1B1), // petang
+    75: Color(0xFF0D1F75), // malam
+    100: Color(0xFFFFE4B1) //pagi
+  },
+);
+
+ColorSwatch skyColorsSecondary = ColorSwatch(
+  1,
+  {
+    0: Color(0xFFFFB42A), //pagi
+    25: Color(0xFF62B1CE), // tengah hari
+    50: Color(0xFFAC4141), // petang
+    75: Color(0xFF101F6D), // malam
+    100: Color(0xFFFFB42A) //pagi
+  },
+);
--- lib/panoramic/panoramic_painter.dart
+++ lib/panoramic/panoramic_painter.dart
@@ -1,9 +1,12 @@
+import 'package:fluteramic_clock/panoramic/color_const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 class PanoramicPainter extends CustomPainter {
   double percentage;
   PanoramicPainter(this.percentage);
+  double nightPercentage;
+  double dayPercentage;
 
   _drawSky(Canvas canvas, Size size) {
     var sky = Offset.zero & Size(size.width, size.height / 2);
@@ -11,8 +14,20 @@ class PanoramicPainter extends CustomPainter {
       begin: Alignment.topCenter,
       end: Alignment.bottomCenter,
       colors: [
-        Color.lerp(Color(0xFFB1EAFF), Colors.indigo, percentage),
-        Color.lerp(Color(0xFFA6FFEF), Colors.red, percentage)
+        Color.lerp(
+            skyColorsPrimary[(percentage * 100) - ((percentage * 100) % 25)],
+            skyColorsPrimary[
+                (percentage * 100) - ((percentage * 100) % 25) + 25 < 100
+                    ? (percentage * 100) - ((percentage * 100) % 25) + 25
+                    : 0],
+            calculateSessionPecentage),
+        Color.lerp(
+            skyColorsSecondary[(percentage * 100) - ((percentage * 100) % 25)],
+            skyColorsSecondary[
+                (percentage * 100) - ((percentage * 100) % 25) + 25 < 100
+                    ? (percentage * 100) - ((percentage * 100) % 25) + 25
+                    : 0],
+            calculateSessionPecentage),
       ],
       stops: [0, 1],
     );
@@ -23,14 +38,15 @@ class PanoramicPainter extends CustomPainter {
   }
 
   _drawSun(Canvas canvas, Size size) {
+    dayPercentage = ((percentage) / 0.50);
     var sun = Offset.zero & size;
     var sunGradient = RadialGradient(
-      center: Alignment((percentage) - 0.25,
-          _parabolicYValue(percentage) + 0.2), // added offset
-      radius: 1 - (0.5 * percentage),
+      center: Alignment((dayPercentage) - 0.25,
+          _parabolicYValue(dayPercentage) + 0.2), // added offset
+      radius: 1 - (0.5 * dayPercentage),
       colors: [
-        Color.lerp(Color(0xFFFFFFFF), Colors.red, percentage),
-        Color.lerp(Color(0xFFFFC400), Colors.transparent, percentage),
+        Color.lerp(Color(0xFFFFFFFF), Colors.red, dayPercentage),
+        Color.lerp(Color(0xFFFFC400), Colors.transparent, dayPercentage),
         Colors.transparent
       ],
       stops: [0.1, 0.09, 1],
@@ -143,6 +159,33 @@ class PanoramicPainter extends CustomPainter {
           ..shader = mountainGradient.createShader(mountainShaderContainer));
   }
 
+  _drawMoon(Canvas canvas, Size size) {
+    nightPercentage = ((percentage - 0.25) / 0.80);
+    var moon = Offset.zero & size;
+    var moonGradient = RadialGradient(
+      center: Alignment(0.38, -0.72 * percentage - 0.25), // added offset
+      radius: 0.12,
+      colors: [
+        Colors.transparent,
+        Color.fromRGBO(255, 255, 46, nightPercentage > 0 ? nightPercentage : 0),
+      ],
+      stops: [0.5, 0.4],
+    );
+    canvas.drawCircle(
+        Offset.zero.translate(size.width / 1.5, size.height / 6),
+        70,
+        Paint()
+          ..strokeWidth = 30
+          ..strokeCap = StrokeCap.round
+          ..shader = moonGradient.createShader(moon)
+          ..blendMode = BlendMode.hardLight);
+  }
+
+  get calculateSessionPecentage {
+    var sectionVal = ((percentage * 100) - ((percentage * 100) % 25)).toInt();
+    return (percentage - (sectionVal / 100)) / 0.25;
+  }
+
   _drawSmallLand(Canvas canvas, Size size) {
     var land = Path();
     var mountainShaderContainer =
@@ -175,46 +218,7 @@ class PanoramicPainter extends CustomPainter {
     const b = 1;
     var yVal = (-4 * (-0.9) / ((a - b) * (a - b))) *
         ((xVal * xVal) - ((a + b) * xVal) + (a * b));
-    print(xVal.toString() + ',' + yVal.toString());
     return yVal;
-  }
-
-  _drawShip(Canvas canvas, Size size) {
-    var sea = Offset.zero.translate(size.width * 0.8, size.height * 0.7) &
-        Size(size.width * 0.08, size.height * 0.04);
-
-    var seaGradient = LinearGradient(
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-      colors: [Color(0xFFFFFFFF), Color(0xFFFFFFFFF)],
-      stops: [0, .8],
-    );
-    canvas.drawRect(
-      sea,
-      Paint()..shader = seaGradient.createShader(sea),
-    );
-  }
-
-// TODO:Remove debug text
-  _drawDebugText(Canvas canvas, Size size) {
-    final textStyle = TextStyle(
-      color: Colors.black,
-      fontSize: 30,
-    );
-    final textSpan = TextSpan(
-      text: percentage.toString(),
-      style: textStyle,
-    );
-    final textPainter = TextPainter(
-      text: textSpan,
-      textDirection: TextDirection.ltr,
-    );
-    textPainter.layout(
-      minWidth: 0,
-      maxWidth: size.width,
-    );
-    final offset = Offset(50, 100);
-    textPainter.paint(canvas, offset);
   }
 
   @override
@@ -223,11 +227,10 @@ class PanoramicPainter extends CustomPainter {
     _drawSun(canvas, size);
     _drawSea(canvas, size);
     _drawMountain(canvas, size);
-    // _drawShip(canvas, size);
     _drawSmallMountain(canvas, size);
     _drawLand(canvas, size);
     _drawSmallLand(canvas, size);
-    // _drawDebugText(canvas, size);
+    _drawMoon(canvas, size);
   }
 
   @override
